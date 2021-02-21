ebc88cf6539fd067d6c945fb92864640a7b5212d
Ewa Klosinska <ewa.klosinska2@gmail.com>
2021-02-21 16:14:40 +0100

    first commit

---
+++ main.js
@@ -0,0 +1,15 @@
+const amount = document.querySelector('input#amount');
+const select = document.getElementById('currency');
+const result = document.querySelector('div.result');
+const btn = document.querySelector('button');
+
+function selectElem(event) {
+        event.preventDefault();
+
+    axios.get(`http://api.nbp.pl/api/exchangerates/rates/A/${select.value.toLowerCase()}/`).then(function(response) {
+            let rate = response.data.rates[0].mid;
+            result.innerText = (amount.value * rate).toFixed(2) + " PLN";
+    })
+}
+
+btn.addEventListener('click', selectElem);
